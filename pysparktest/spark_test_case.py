ea9c58f5e424ad4116f15f6b12493503dead78e6
Joe Heffer <j.heffer@sheffield.ac.uk>
2021-04-08 10:54:22 +0100

    fix typo

--- pysparktest/spark_test_case.py
+++ pysparktest/spark_test_case.py
@@ -7,3 +7,14 @@ class SparkTestCase(unittest.TestCase):
     def setUp(self):
         # Create Spark context
         self.sc = pyspark.sql.SparkSession.builder.getOrCreate()
+
+        self.df = self.sc.createDataFrame(data=list())
+
+    def test_print_schema(self):
+        self.df.printSchema()
+
+    def test_show(self):
+        self.df.show()
+
+    def test_collect(self):
+        self.df.collect()
