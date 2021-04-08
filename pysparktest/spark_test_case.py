61cf8c04024266603f98bd002c52b44b44e3205f
Joe Heffer <j.heffer@sheffield.ac.uk>
2021-04-08 11:07:04 +0100

    use one spark session

--- pysparktest/spark_test_case.py
+++ pysparktest/spark_test_case.py
@@ -3,11 +3,14 @@ import unittest
 import pyspark.sql
 from pyspark.sql.types import StructType
 
+# Create Spark session (only one can exist per process)
+# https://stackoverflow.com/a/41513805
+SPARK_SESSION = pyspark.sql.SparkSession.builder.getOrCreate()
+
 
 class SparkTestCase(unittest.TestCase):
     def setUp(self):
-        # Create Spark session
-        self.session = pyspark.sql.SparkSession.builder.getOrCreate()
+        self.session = SPARK_SESSION
 
         self.df = self.session.createDataFrame(data=list(),
                                                schema=StructType(list()))
